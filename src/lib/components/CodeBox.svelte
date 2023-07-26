3c199c922ccf4dd9aadbb483c13c331b1514d844
EmilienKopp <emilien.kopp@gmail.com>
2023-07-26 17:56:34 +0900

    Fixed placement on small screens and opacity

--- src/lib/components/CodeBox.svelte
+++ src/lib/components/CodeBox.svelte
@@ -17,8 +17,8 @@
 
    
     {#key $typingBlockContent}
-    <div class="opacity-50 z-[1] pointer-events-none select-none">
-        <pre class="{$typingBlockContent ? "pt-[52vh] lg:mt-2 md:w-4/6 text-xs lg:text-sm xl:text-xl" : "text-xs lg:text-md xl:text-4xl"} rounded-md px-4 pt-12 block w-full ">
+    <div class="opacity-80 z-[1] pointer-events-none select-none">
+        <pre class="{$typingBlockContent ? "pt-[30vh] sm:pt-[52vh] lg:mt-2 md:w-4/6 text-xs lg:text-sm xl:text-xl" : "text-xs lg:text-md xl:text-4xl"} rounded-md px-4 pt-12 block w-full ">
         <code class="code-panel sm:text-xs md:text-lg lg:text-xl language-javascript">
             <span id="typing" class="type" style="--n:478" bind:this={typingBlock}>
                 {#if $typingBlockContent == ""}
