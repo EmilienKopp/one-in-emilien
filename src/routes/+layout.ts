08fe72c4b910616c8cfe2acfe38c6bc4b157b204
EmilienKopp <emilien.kopp@gmail.com>
2023-08-12 13:12:01 +0900

    added auth control to layout.ts

---
+++ src/routes/+layout.ts
@@ -0,0 +1,21 @@
+import { PUBLIC_SUPABASE_ANON_KEY, PUBLIC_SUPABASE_URL } from '$env/static/public'
+
+import { createSupabaseLoadClient } from '@supabase/auth-helpers-sveltekit'
+import { invalidate } from '$app/navigation'
+
+export const load = async ({ fetch, data, depends }: any) => {
+  depends('supabase:auth')
+
+  const supabase = createSupabaseLoadClient({
+    supabaseUrl: PUBLIC_SUPABASE_URL,
+    supabaseKey: PUBLIC_SUPABASE_ANON_KEY,
+    event: { fetch },
+    serverSession: data.session,
+  })
+
+  const {
+    data: { session },
+  } = await supabase.auth.getSession()
+
+  return { supabase, session }
+}
