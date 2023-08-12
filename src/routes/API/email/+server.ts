aea2d17aaf4b8056a7891ac91434216f8274297c
EmilienKopp <emilien.kopp@gmail.com>
2023-08-12 13:13:08 +0900

    contact form auto email

--- src/routes/API/email/+server.ts
+++ src/routes/API/email/+server.ts
@@ -1,10 +1,38 @@
-import { EMAIL } from "$env/static/private";
+import { CONTACT_EMAIL, EMAIL, SENDGRID_API_KEY } from "$env/static/private";
+
 import { json } from "@sveltejs/kit";
+import sgMail from "@sendgrid/mail";
+
+sgMail.setApiKey(SENDGRID_API_KEY);
 
 export async function GET({ params, request }: any) {
     const data = {
         email: EMAIL
     }
     console.log(JSON.stringify(data));
+    return json(data);
+}
+
+export async function POST({ params, request }: any) {
+    const data = await request.json();
+    console.log(JSON.stringify(data));
+
+    const emailMessage = {
+        to: EMAIL,
+        from: CONTACT_EMAIL,
+        subject: "New Contact Form Submission",
+        text: `Name: ${data.customer_name}
+                \nCompany: ${data.company_name}
+                \nEmail: ${data.email}
+                \nInquiry: ${data.inquiry}
+                \nMessage: ${data.message ?? "None"}
+                \nTime: ${new Date().toLocaleString()}    
+            `
+    }
+
+    const output = await sgMail.send(emailMessage);
+
+    console.log(output);
+
     return json(data);
 }
