8723c020d1de1031ef658ae2cb3f13508fe025b3
EmilienKopp <emilien.kopp@gmail.com>
2023-08-27 03:34:28 +0900

    feat: added Asphelt to showcase =)

--- svelte.config.js
+++ svelte.config.js
@@ -15,6 +15,17 @@ const config = {
     // See https://kit.svelte.dev/docs/adapters for more information about adapters.
     adapter: adapter(),
   },
+  vite: {
+    server: {
+        proxy: {
+            '/API/mock': {
+                target: 'http://localhost:5174',
+                changeOrigin: true,
+                ws: true
+            }
+        }
+    }
+}
 };
 
 export default config;
