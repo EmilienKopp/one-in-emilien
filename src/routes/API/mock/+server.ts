f993861d5b35d778a1806d91282b41e80b9b9bcb
EmilienKopp <emilien.kopp@gmail.com>
2023-08-28 16:37:59 +0900

    fix: preflight headers

--- src/routes/API/mock/+server.ts
+++ src/routes/API/mock/+server.ts
@@ -4,6 +4,7 @@ export async function OPTIONS ({ params, request }: any) {
     const headersWithCORS = {
         'Access-Control-Allow-Origin': '*',
         'Access-Control-Allow-Methods': 'POST, GET, OPTIONS',
+        'Access-Control-Allow-Headers': 'Content-Type',
     }
     return new Response(null, { headers: headersWithCORS });
 }
