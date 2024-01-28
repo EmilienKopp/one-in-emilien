5320889f81258d1250e04d85c9927f1ca9c50d1e
EmilienKopp <emilien.kopp@gmail.com>
2024-01-29 00:47:01 +0900

    Refactor line webhook handler

--- src/routes/API/line/+server.ts
+++ src/routes/API/line/+server.ts
@@ -5,15 +5,16 @@ import { createClient } from "@supabase/supabase-js";
 export async function POST({ params, request }: any) {
     const supabase = createClient(PUBLIC_SUPABASE_URL, PUBLIC_SUPABASE_ANON_KEY);
     console.log("RECEIVER LINE WEBHOOK", params, request);
-    const {events} = request.json();
-    for(const {message,source: {userId}} of events){
+    const body = request.json();
+    console.log(body, body.events);
+    // for(const {message,source: {userId}} of events){
         
-        const {error} = await supabase.from("line_inquiries").insert({
-            user_line_id: userId,
-            message: message.text ? message.text : message.type
-        }).select();
-        if(error){
-            console.error("RECEIVER LINE WEBHOOK", error);
-        }
-    }
+    //     const {error} = await supabase.from("line_inquiries").insert({
+    //         user_line_id: userId,
+    //         message: message.text ? message.text : message.type
+    //     }).select();
+    //     if(error){
+    //         console.error("RECEIVER LINE WEBHOOK", error);
+    //     }
+    // }
 }
