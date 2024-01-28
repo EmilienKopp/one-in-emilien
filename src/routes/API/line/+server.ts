0d9ee14513075944e9eac030cc4b701f1ff2a587
EmilienKopp <emilien.kopp@gmail.com>
2024-01-29 00:41:53 +0900

    feat: line webhook userId extraction test

--- src/routes/API/line/+server.ts
+++ src/routes/API/line/+server.ts
@@ -1,7 +1,19 @@
+import { PUBLIC_SUPABASE_ANON_KEY, PUBLIC_SUPABASE_URL } from "$env/static/public";
 
+import { createClient } from "@supabase/supabase-js";
 
 export async function POST({ params, request }: any) {
-   
+    const supabase = createClient(PUBLIC_SUPABASE_URL, PUBLIC_SUPABASE_ANON_KEY);
     console.log("RECEIVER LINE WEBHOOK", params, request);
-    console.log(await request.json());
+    const {events} = request.body();
+    for(const {message,source: {userId}} of events){
+        
+        const {error} = await supabase.from("line_inquiries").insert({
+            user_line_id: userId,
+            message: message.text ? message.text : message.type
+        }).select();
+        if(error){
+            console.error("RECEIVER LINE WEBHOOK", error);
+        }
+    }
 }
